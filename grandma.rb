57676e69631007211c9ff0e5c3583b468120da74
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-29 17:46:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(response)
   if response == "I LOVE YOU GRANDMA"
-    puts "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN"
   end
 end
 
