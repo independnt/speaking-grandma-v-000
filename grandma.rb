4dd695f577ed474946e79356745f0d751972f035
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-29 17:46:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,9 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(response)
+  if response == "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
